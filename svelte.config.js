deec8e432c96bd9d301bb63302806b795d3ceaf1
janithwanni <janithcwanni@gmail.com>
2025-03-28 01:33:25 +1100

    switch to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+    adapter: adapter({edge: false})
 	}
 };
 
